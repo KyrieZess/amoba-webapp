372400a154af77fc6db7042da87aea6bae10b04a
F. Anita <ZKyrie666@gmail.com>
2023-06-01 10:21:42 +0200

    component globally use

--- src/main.js
+++ src/main.js
@@ -17,6 +17,13 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
+// Import styles
+import './styles/index.css'
+// Import BaseComponents
+import BaseComponents from './components/baseComponents'
+// Add for globally using the registered components
+Vue.use(BaseComponents);
+
 new Vue({
   router,
   store,
