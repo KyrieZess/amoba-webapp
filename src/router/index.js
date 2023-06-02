365a6f422a2ce8de5a1cc37cc200466c40396caf
F. Anita <ZKyrie666@gmail.com>
2023-06-03 00:33:11 +0200

    set router

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,66 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import About from '../views/About.vue'
+import LoggedRouterView from '../views/LoggedRouterView.vue'
+import NotLoggedRouterView from '../views/NotLoggedRouterView.vue'
+
+import { AuthLogic } from '../logic/auth'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "LoggedRouterView",
+    component: LoggedRouterView,
+    children: [
+      {
+        path: '/',
+        name: 'Home',
+        components: { loggedView: Home },
+      },
+      {
+        path: '/about',
+        name: 'About',
+        components: { loggedView: About },
+      },
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
+    path: "/",
+    name: "NotLoggedRouterView",
+    component: NotLoggedRouterView,
+    children: [
+      {
+        path: '/login',
+        name: 'Login',
+        components: { notLoggedView: Login },
+      }
+    ]
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: routes
 })
+
+router.beforeEach((to, from, next) => {
+  const tokenIsValid = AuthLogic.TokenIsValid();
+
+  console.log(from.name)
+  console.log(to.name)
+  console.log(tokenIsValid)
+
+  if (!tokenIsValid && to.name != "Login" && from.name != null) {
+    next("login");
+  } else if (tokenIsValid || from.name == null) {
+    next()
+  }
+});
 
 export default router
