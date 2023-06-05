22ba6036cb0996090117187f7dd1085a803c81de
F. Anita <ZKyrie666@gmail.com>
2023-06-05 14:22:21 +0200

    unused store remove

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
@@ -28,6 +27,5 @@ Vue.prototype.$baseUrl = "https://dummyjson.com/"
 
 new Vue({
   router,
-  store,
   render: function (h) { return h(App) }
 }).$mount('#app')
