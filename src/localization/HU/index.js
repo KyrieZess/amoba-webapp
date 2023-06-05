a32e0066723342167fcee6b0875aec2c2b1df94d
F. Anita <ZKyrie666@gmail.com>
2023-06-05 17:54:28 +0200

    create hun dictionaries

--- src/localization/HU/index.js
+++ src/localization/HU/index.js
@@ -1,2 +1,17 @@
+import Game from './game.json'
+import GameTable from './gameTable.json'
+import WinnerModal from './winnerModal.json'
+import GameSetting from './gameSetting.json'
+import PlayerSetting from './playerSetting.json'
+import Login from './login.json'
+import Menu from './menu.json'
+
 export default {
+    Game: Game,
+    GameTable: GameTable,
+    WinnerModal: WinnerModal,
+    GameSetting: GameSetting,
+    PlayerSetting: PlayerSetting,
+    Login: Login,
+    Menu: Menu
 }
